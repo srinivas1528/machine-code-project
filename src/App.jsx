36230899ffee97a05ae18529fb7a43b664bb6b32
[Srinivas Potru] <[srinivaskalyan28@gmail.com]>
2025-06-24 17:49:41 +0530

    Add stop watch utility

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react"
+import './App.css';
+import useTimer from "./hooks/UseTimer";
+
+
+function App() {
+
+
+  const [active, setStarted] = useState(false);
+
+  const [paused, setPaused] = useState(false);
+
+  const [completed, setCompleted] = useState(true);
+
+  const { current } = useTimer(10, active, paused, completed);
+
+  // simple change states and let the useTimer hook handle the timer logic
+  const handleStartOrResumeClick = () => {
+    setStarted(true);
+    setPaused(false);
+    setCompleted(false);
+  }
+
+
+  const handlePauseClick = () => {
+    setPaused(true);
+  }
+
+
+  const handleCompleteClick = () => {
+    setCompleted(true);
+    setStarted(false);
+    setPaused(false);
+  }
+
+
+  return (
+    <>
+      <h1>React Timer</h1>
+      <div className="card">
+        {/* Start/Resume Button */}
+        <button onClick={() => handleStartOrResumeClick()} disabled={active && !paused}>
+          {paused ? "Resume" : "Start"}
+        </button>
+
+       {/* Pause Button */}
+        <button onClick={() => handlePauseClick()} disabled={!active || paused}>
+          Pause
+        </button>
+
+        {/* Timer Display */}
+        <div>Current: {current}</div>
+
+        {/* Stop Button */}
+        <button onClick={() => handleCompleteClick()} disabled={completed}>
+          Stop
+        </button>
+
+      </div>
+    </>
+  )
+}
+
+export default App;
---
+++ src/hooks/UseTimer.js
@@ -0,0 +1,34 @@
+import  { useEffect, useState } from "react"
+
+
+
+const useTimer = ( initialValue, active, paused, completed ) => {
+
+    const [current, setCurrent] = useState(initialValue);
+
+
+    useEffect(() => {
+
+        let handler;
+
+        if (completed) {
+            // Resets timer when stop button is clicked
+           setCurrent(initialValue);
+        } else if (active && !paused) {
+            handler = setInterval(() => {
+
+                // Increment timer when start button is clicked
+                setCurrent((prev) => prev + 1);
+
+            }, 1000)
+        }
+        // Clean up interval
+        return () => clearInterval(handler)
+    }, [active, paused, completed, initialValue])
+
+
+    return { current, setCurrent }
+
+}
+
+export default useTimer;
